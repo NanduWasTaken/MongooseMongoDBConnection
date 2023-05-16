889a4230c364bdca8b7ba039df9be16b74830a41
Nandu <nanduwastaken@gmail.com>
2023-05-16 16:38:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.DPASS, {
+mongoose.connect('Super Secrect MongoDB URI Here', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
